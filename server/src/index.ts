19e6e3df6c289e5609d2d9bbfcdd4c60e2d3439f
the_glimmered_ones <testtesttestboi69@gmail.com>
2026-09-14 14:31:25 -0500

    h

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import Elysia from "elysia";
-//import { html } from '@elysia/html'
+import { html } from '@elysia/html'
 import { staticPlugin } from '@elysia/static'
 import { ShipTypes, PlayerLocation, GlobalClientLocation } from "@shared/Consts";
 import { ClientPacketTypes, ServerPacketTypes, ClientPacket, ServerPacket } from "@shared/PacketTypes";
@@ -28,7 +28,7 @@ const clients: Client[] = []
 const nameList: string[] = []
 
 new Elysia()
-    // .use(html()) // only needed for local server, cloudflare has its own way of serving the page
+    .use(html()) // only needed for local server, cloudflare has its own way of serving the page
     .use(
         staticPlugin({
             assets: 'client/build',
