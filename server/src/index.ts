61df3b595db175ff2342eb82d50048abdebfe9a1
the_glimmered_ones <testtesttestboi69@gmail.com>
2026-09-14 16:07:08 -0500

    removed await

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,7 +28,7 @@ const clients: Client[] = []
 const nameList: string[] = []
 console.log("server file reached")
 
-await new Elysia()
+new Elysia()
     .use(html()) // only needed for local server, cloudflare has its own way of serving the page
     .use(
         staticPlugin({
