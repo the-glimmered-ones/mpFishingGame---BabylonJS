2a328ec121c2440ceacc16ccde0c05b4a7d32efd
the_glimmered_ones <sbthompson429@gmail.com>
2026-09-25 13:32:04 -0500

    started ui

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,41 +28,28 @@ console.log("server file reached")
 const HOST_NAME = "localhost"
 const PORT = 2323
 const TEXTURE_PATH = "./shared/textures/"
-const SUB_TEXTURE_PATH = "./shared/textures/TropicalSunnyDay/"
+
+async function serveRes(url: string, subfolder: string = ""){
+    let filePath = url.slice(url.indexOf("/textures/" + subfolder) + ("/textures/" + subfolder).length)
+    console.log(url, filePath)
+    const foundTexture = (await readdir(TEXTURE_PATH + subfolder, {withFileTypes: true})).find((texture) => { return texture.name == filePath}) //doesn't work for subfolders
+    //console.log(foundTexture)
+    if (foundTexture){
+        return new Response(Bun.file(TEXTURE_PATH + subfolder + filePath))
+    }
+    else{
+        return new Response("404 not found")
+    }
+}
 
 Bun.serve({
     port: PORT,
     hostname: HOST_NAME,
     routes: {
         "/": page,
-        "/textures/TropicalSunnyDay/*": async ( {url} ) => {
-    
-            let filePath = url.slice(url.indexOf("/textures/TropicalSunnyDay/") + "/textures/TropicalSunnyDay/".length)
-            console.log(filePath)
-            const foundTexture = (await readdir(SUB_TEXTURE_PATH, {withFileTypes: true})).find((texture) => { return texture.name == filePath}) //doesn't work for subfolders
-            console.log(foundTexture)
-            if (foundTexture){
-                return new Response(Bun.file(SUB_TEXTURE_PATH + filePath))
-            }
-            else{
-                return new Response("404 not found")
-            }
-        },
-        "/textures/*": async ( {url} ) => {
-            
-            //get text after textures: index of "/textures/" plus its length
-            let filePath = url.slice(url.indexOf("/textures/") + "/textures/".length)
-            console.log(filePath)
-            //https://developer.mozilla.org/en-US/docs/Web/API/FileSystemDirectoryEntry/createReader - use this 
-            const foundTexture = (await readdir(TEXTURE_PATH, {withFileTypes: true})).find((texture) => { return texture.name == filePath}) //doesn't work for subfolders
-            console.log(foundTexture)
-            if (foundTexture){
-                return new Response(Bun.file(TEXTURE_PATH + filePath))
-            }
-            else{
-                return new Response("404 not found")
-            }
-        }
+        "/textures/ui/*": ({url}) => {return serveRes(url, "ui/")},
+        "/textures/TropicalSunnyDay/*": ({url}) => {return serveRes(url, "TropicalSunnyDay/")},
+        "/textures/*": ({url}) => {return serveRes(url)},
     },
     async fetch(req, server) {
         // upgrade the request to a WebSocket
